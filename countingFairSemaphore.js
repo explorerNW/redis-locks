69b7e6ba5cf229cad5db4e6866519987e1ec3d5a
explorerNW <superexplorernw@163.com>
2022-11-12 19:07:35 +0800

    feat(husky): add husky & lint

--- countingFairSemaphore.js
+++ countingFairSemaphore.js
@@ -8,23 +8,23 @@ function countingFairSemaphore(name, limit = 10, timeout = 100000) {
     const counter = semaphore + ":counter";
     const defer = new Defer();
     const now = Date.now();
-    client.zremrangebyscore(semaphore, "-inf", Date.now() - timeout, (err, response0) => {
-        client.zinterstore(owner, 2, owner, semaphore, "weights", 1, 0, (err, response1) => {
+    client.zremrangebyscore(semaphore, "-inf", Date.now() - timeout, () => {
+        client.zinterstore(owner, 2, owner, semaphore, "weights", 1, 0, (err) => {
             errorHandler(err);
             client.incr(counter, (err, counterValue) => {
                 errorHandler(err);
-                client.zadd(semaphore, now, identifier, (err, response3) => {
+                client.zadd(semaphore, now, identifier, (err) => {
                     errorHandler(err);
-                    client.zadd(owner, counterValue, identifier, (err, response4) => {
+                    client.zadd(owner, counterValue, identifier, (err) => {
                         errorHandler(err);
                         client.zrank(owner, identifier, (err, rank) => {
                             errorHandler(err);
                             if (rank < limit) {
                                 defer.resolve(identifier);
                             } else {
-                                client.zrem(semaphore, identifier, (err, response5) => {
+                                client.zrem(semaphore, identifier, (err) => {
                                     errorHandler(err);
-                                    client.zrem(owner, identifier, (err, response6) => {
+                                    client.zrem(owner, identifier, (err) => {
                                         errorHandler(err);
                                         defer.resolve(false);
                                     });
@@ -45,7 +45,7 @@ function refreshFairSemaphore(name, sema) {
     client.zadd(lock, sema, (err, response0) => {// sorted set
         errorHandler(err);
         if (response0) {
-            client.zrem(lock, sema, (err, response1) => {
+            client.zrem(lock, sema, (err) => {
                 errorHandler(err);
                 defer.resolve(false);
             });
@@ -59,9 +59,9 @@ function refreshFairSemaphore(name, sema) {
 function releaseFairSemaphore(name, sema) {
     const lock = "lock:" + name;
     const owner = lock + ":owner";
-    client.zrem(owner, sema, (err, response0) => {
+    client.zrem(owner, sema, (err) => {
         errorHandler(err);
-        client.zrem(lock, sema, (err, response1) => {
+        client.zrem(lock, sema, (err) => {
             errorHandler(err);
         });
     });
